83483a3c43c2cf7e58ba5fb5a1016032322d9c2f
IceColdMoon <46549597+lceCoIdMoon@users.noreply.github.com>
2021-12-15 14:41:06 -0500

    linux

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-import 'dart:io';
-
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
-
-import 'package:path/path.dart' as p;
 
 void main() async {
   runApp(App());
