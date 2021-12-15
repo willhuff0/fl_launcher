4e09bcf09aacd45c455383cea2ca254d7003f28c
Will Huffman <wehuffman15@gmail.com>
2021-12-15 13:57:22 -0500

    initial

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:flutter/material.dart';
+
+import 'package:path/path.dart' as p;
+
+void main() async {
+  runApp(App());
+
+  doWhenWindowReady(() {
+    appWindow.minSize = Size(600, 450);
+    appWindow.size = Size(800, 600);
+    appWindow.alignment = Alignment.center;
+    appWindow.title = 'FLLauncher';
+    appWindow.show();
+  });
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Home(), debugShowCheckedModeBanner: false);
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
