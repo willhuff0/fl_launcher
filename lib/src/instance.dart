4ccd703f6d91e0f7f9d4a158134371cf6e29fdc5
lceCoIdMoon <wehuffman15@gmail.com>
2022-03-20 12:10:26 -0400

    updates

--- lib/src/instance.dart
+++ lib/src/instance.dart
@@ -43,34 +43,39 @@ class VanillaInstance extends Instance {
     final json = await getVersionJson(version);
 
     // getAssets
-    print('Fetching assets...');
-    final path = p.join(root, 'assets', 'indexes');
-    await Directory(path).create();
+    {
+      print('Fetching assets...');
+      final path = p.join(root, 'assets', 'indexes');
+      await Directory(path).create();
 
-    final assetIndexPath = p.join(path, '${json['assetIndex']['id']}.json');
-    final assetIndex = await http.get(Uri.parse(json['assetIndex']['url'])).then((value) => value.body);
-    await File(assetIndexPath).writeAsString(assetIndex);
-    final assetindexes = jsonDecode(assetIndex)['objects'] as Map<String, dynamic>;
-    print('> Got assetIndex.json');
-    print('> Downloading ${assetindexes.length} assets');
+      final assetIndexPath = p.join(path, '${json['assetIndex']['id']}.json');
+      final assetIndex = await http.get(Uri.parse(json['assetIndex']['url'])).then((value) => value.body);
+      await File(assetIndexPath).writeAsString(assetIndex);
+      final assetindexes = jsonDecode(assetIndex)['objects'] as Map<String, dynamic>;
+      print('> Got assetIndex.json');
+      print('> Downloading ${assetindexes.length} assets');
 
-    final downloadFutures = <Future>[];
+      final downloadFutures = <Future>[];
 
-    final assetsPath = p.join(root, 'assets', 'objects');
-    Future downloadFile(String hash) async {
-      final subhash = hash.substring(0, 2);
-      final path = p.join(assetsPath, subhash, hash);
-      final url = 'https://resources.download.minecraft.net/${subhash}/${hash}';
-      await http.get(Uri.parse(url)).then((response) => File(path).create(recursive: true).then((file) => file.writeAsBytes(response.bodyBytes)));
+      final assetsPath = p.join(root, 'assets', 'objects');
+      Future downloadFile(String hash) async {
+        final subhash = hash.substring(0, 2);
+        final path = p.join(assetsPath, subhash, hash);
+        final url = 'https://resources.download.minecraft.net/${subhash}/${hash}';
+        await http.get(Uri.parse(url)).then((response) => File(path).create(recursive: true).then((file) => file.writeAsBytes(response.bodyBytes)));
+      }
+
+      assetindexes.forEach((key, value) => downloadFutures.add(downloadFile(value['hash'])));
+      await Future.wait(downloadFutures);
+      print('> Done.');
     }
-
-    assetindexes.forEach((key, value) => downloadFutures.add(downloadFile(value['hash'])));
-    await Future.wait(downloadFutures);
-    print('> Done.');
     //
 
     // getJar
-
+    {
+      final path = p.join(root, 'versions', version);
+      await http.get(Uri.parse(url)).then((response) => File(path).create(recursive: true).then((file) => file.writeAsBytes(response.bodyBytes)));
+    }
     //
   }
 }
